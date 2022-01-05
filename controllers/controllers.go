cbb3bfb80e4d068ef222a368c5c528417ca79ad8
Kody Low <kodylow7@gmail.com>
2022-01-05 14:39:08 -0500

    added gorilla/schema for form parse

--- controllers/controllers.go
+++ controllers/controllers.go
@@ -1,8 +1,11 @@
 package controllers
 
 import (
+	"fmt"
 	"lenslocked/views"
 	"net/http"
+
+	"github.com/gorilla/schema"
 )
 
 //NewUsers creates a new Users controller.
@@ -20,8 +23,30 @@ type Users struct {
 }
 
 //New renders the new user page
+//
+//Get /signup
 func (u *Users) New(w http.ResponseWriter, r *http.Request) {
 	if err := u.NewView.Render(w, nil); err != nil {
 		panic(err)
 	}
+}
+
+type SignupForm struct {
+	Email				string		`schema:"email"`
+	Password		string		`schema:"password"`
+}
+
+//Create creates a new user account from the signup form on submit
+//
+//Post /signup
+func (u *Users) Create(w http.ResponseWriter, r *http.Request) {
+	if err := r.ParseForm(); err != nil {
+		panic(err)
+	}
+	dec := schema.NewDecoder()
+	var form SignupForm
+	if err := dec.Decode(&form, r.PostForm); err != nil {
+		panic(err)
+	}
+	fmt.Fprintln(w, form)
 }
--- main.go
+++ main.go
@@ -43,9 +43,10 @@ func main() {
 
 	r := mux.NewRouter()
 
-	r.HandleFunc("/", home)
-	r.HandleFunc("/contact", contact)
-	r.HandleFunc("/signup", usersC.New)
+	r.HandleFunc("/", home).Methods("GET")
+	r.HandleFunc("/contact", contact).Methods("GET")
+	r.HandleFunc("/signup", usersC.New).Methods("GET")
+	r.HandleFunc("/signup", usersC.Create).Methods("POST")
 	r.HandleFunc("/faq", faq)
 	r.NotFoundHandler = http.HandlerFunc(err404)
 
