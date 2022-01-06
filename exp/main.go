dd81b911d72a79332f957685a6b073e8781886f3
Kody Low <kodylow7@gmail.com>
2022-01-06 15:07:51 -0500

    populated with some fake data

--- exp/main.go
+++ exp/main.go
@@ -25,14 +25,42 @@ func main() {
 
 	fmt.Println("Connected!")
 	defer db.Close()
-
-	var id int
-	err = db.QueryRow(`
-		INSERT INTO users(name, email) VALUES($1, $2) RETURNING id
-		`, 
-		"Kody Low", "kodylow@me.com").Scan(&id)
-	if err != nil {
-		panic(err)
+	for i := 1; i <= 6; i ++ {
+		userID := 1
+		if i > 3 {
+			userID = 2
+		}
+		amount := i * 100
+		description := fmt.Sprintf("Test %d", userID)
+		_, err = db.Query(`
+			INSERT INTO orders(user_id, amount, description)
+			VALUES($1, $2, $3)`, userID, amount, description)
+		if err != nil {
+			panic(err)
+		}
 	}
-	fmt.Println("id is...", id)
 }
+
+// type User struct {
+// 		ID int
+// 		Name string
+// 		Email string
+// 	}
+
+// 	var users []User
+// 	rows, err := db.Query(`
+// 		SELECT id, name, email
+// 		FROM users`)
+// 	if err != nil {
+// 		panic(err)
+// 	}
+// 	defer rows.Close()
+// 	for rows.Next() {
+// 		var user User
+// 		err := rows.Scan(&user.ID, &user.Name, &user.Email)
+// 		if err != nil {
+// 			panic(err)
+// 		}
+// 		users = append(users, user)
+// 	}
+// 	fmt.Println(users)
