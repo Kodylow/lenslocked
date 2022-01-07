78ee4f27abc32b0b9573b46c44002100e6a8fd9d
Kody Low <kodylow7@gmail.com>
2022-01-07 16:37:14 -0500

    switching to gorm

--- exp/main.go
+++ exp/main.go
@@ -1,10 +1,10 @@
 package main
 
 import (
-	"database/sql"
 	"fmt"
 
-	_ "github.com/lib/pq"
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/postgres"
 )
 
 const (
@@ -15,31 +15,34 @@ const (
 	dbname = "localdb"
 )
 
+type User struct {
+	gorm.Model
+	Name 	string
+	Email	string `gorm:"not null;unique-index"`
+	Color	string
+}
+
 func main() {
 	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable", host, port, user, password, dbname)
 	
-	db, err := sql.Open("postgres", psqlInfo)
+	db, err := gorm.Open("postgres", psqlInfo)
 	if err != nil {
 		panic(err)
 	}
-
-	fmt.Println("Connected!")
 	defer db.Close()
-	for i := 1; i <= 6; i ++ {
-		userID := 1
-		if i > 3 {
-			userID = 2
-		}
-		amount := i * 100
-		description := fmt.Sprintf("Test %d", userID)
-		_, err = db.Query(`
-			INSERT INTO orders(user_id, amount, description)
-			VALUES($1, $2, $3)`, userID, amount, description)
-		if err != nil {
-			panic(err)
-		}
+
+	db.LogMode(true)
+	db.AutoMigrate(&User{})
+
+	var u User = User{
+		Color: "red",
+		Email: "kody@me.com",
 	}
+
+	db.Where(u).First(&u)
+	fmt.Println(u)
 }
+
 
 // type User struct {
 // 		ID int
@@ -64,3 +67,25 @@ func main() {
 // 		users = append(users, user)
 // 	}
 // 	fmt.Println(users)
+
+// db, err := sql.Open("postgres", psqlInfo)
+// 	if err != nil {
+// 		panic(err)
+// 	}
+
+// 	fmt.Println("Connected!")
+// 	defer db.Close()
+// 	for i := 1; i <= 6; i ++ {
+// 		userID := 1
+// 		if i > 3 {
+// 			userID = 2
+// 		}
+// 		amount := i * 100
+// 		description := fmt.Sprintf("Test %d", userID)
+// 		_, err = db.Query(`
+// 			INSERT INTO orders(user_id, amount, description)
+// 			VALUES($1, $2, $3)`, userID, amount, description)
+// 		if err != nil {
+// 			panic(err)
+// 		}
+// 	// }
--- go.mod
+++ go.mod
@@ -6,6 +6,8 @@ require (
 	github.com/gorilla/mux v1.8.0 // indirect
 	github.com/gorilla/schema v1.2.0 // indirect
 	github.com/howeyc/fsnotify v0.9.0 // indirect
+	github.com/jinzhu/gorm v1.9.16 // indirect
+	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/lib/pq v1.10.4 // indirect
 	github.com/mattn/go-colorable v0.1.12 // indirect
 	github.com/mattn/go-isatty v0.0.14 // indirect
