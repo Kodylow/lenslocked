982ac3cc69db89a29aa7d6c0525df4698dca49bc
Kody Low <kodylow7@gmail.com>
2022-01-08 09:10:51 -0500

    finished user model crud

--- models/user.go
+++ models/user.go
@@ -9,6 +9,7 @@ import (
 
 var (
 	ErrNotFound = errors.New("models: resources not found")
+	ErrInvalidID = errors.New("models: ID provided was invalid")
 )
 
 // NewUserService creates the singleton of the user service
@@ -32,14 +33,31 @@ type UserService struct {
 //3 - nil, otherError
 func (us *UserService) ByID(id uint) (*User, error) {
 	var user User
-	err := us.db.Where("id = ?", id).First(&user).Error
+	db := us.db.Where("id = ?", id)
+	err := first(db, &user)
+	return &user, err
+}
+
+// ByEmail looks up by email
+func (us *UserService) ByEmail(email string) (*User, error) {
+	var user User
+	db := us.db.Where("email = ?", email)
+	err := first(db, &user)
+	return &user, err
+}
+
+//first queries using the provided gorm.db
+//gets the first item returned and places it in dest
+//if nothing found, returns errNotFound
+func first(db *gorm.DB, dest interface{}) error {
+	err := db.First(dest).Error
 	switch err {
 	case nil:
-		return &user, err
+		return nil
 	case gorm.ErrRecordNotFound:
-		return nil, ErrNotFound
+		return ErrNotFound
 	default:
-		return nil, err
+		return err
 	}
 }
 
@@ -52,6 +70,20 @@ func (us *UserService) Create(user *User) error {
 func (us *UserService) Close() error {
 	us.db.Close()
 	return nil
+}
+
+//Update updates user with provided user data
+func (us *UserService) Update(user *User) error {
+	return us.db.Save(user).Error
+}
+
+// Delete deletes user with provided id
+func (us *UserService) Delete(id uint) error {
+	if id == 0 {
+		return ErrInvalidID
+	}
+	user := User{Model: gorm.Model{ID: id}}
+	return us.db.Delete(&user).Error
 }
 
 // DestructiveReset drops tables and automigrates
